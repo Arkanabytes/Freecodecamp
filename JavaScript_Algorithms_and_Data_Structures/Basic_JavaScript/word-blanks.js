dcabfd8d3851929daccf590592a01d02c9bd23a4
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-05-14 00:01:35 +0200

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/word-blanks.js
@@ -0,0 +1,10 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb + "."; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks);
