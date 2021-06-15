6ba50be34704b4ecf3699ea85b983ec7abedc97b
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-06-15 23:37:46 +0200

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/Selecting-from-Many-Options-with-Switch-Statements.js
@@ -0,0 +1,21 @@
+function caseInSwitch(val) {
+  var answer = "";
+  switch(val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+  return answer;  
+}
+
+// Change this value to test
+console.log(caseInSwitch(3));
