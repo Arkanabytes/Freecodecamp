726ff59f3f912647f4eb525c9e63d9d8bc13f880
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-06-15 23:44:44 +0200

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/Adding-a-Default-Option-in-Switch-Statements.js
@@ -0,0 +1,21 @@
+function switchOfStuff(val) {
+  var answer = "";
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+    return answer;  
+  }
+
+// Change this value to test
+console.log(switchOfStuff("c"));
