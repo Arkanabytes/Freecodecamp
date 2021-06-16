8c343b24dfb318b3312ca1ac64878c683f072e57
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-06-16 21:36:58 +0200

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/Accessing-Object-Properties-with-Dot-Notation.js
@@ -0,0 +1,10 @@
+// Setup
+var testObj = {
+  "hatValue": "ballcap",
+  "shirtValue": "jersey"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hatValue;      // Change this line
+var shirtValue = testObj.shirtValue;    // Change this line
