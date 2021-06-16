daa1629c7b617f18d7f5997e509cb2faf9d960e2
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-06-16 21:42:37 +0200

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/Accessing-Object-Properties-with-Bracket-Notation.js
@@ -0,0 +1,10 @@
+// Setup
+var testObj = {
+  "entreeValue": "hamburger",
+  "drinkValue": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["entreeValue"];   // Change this line
+var drinkValue = testObj["drinkValue"];  // Change this line
