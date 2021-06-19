eae32ba751b0a70ad0db2a894c6c814c045279e9
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-06-19 18:22:35 +0200

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/Accessing-Nested-Objects.js
@@ -0,0 +1,15 @@
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+//var gloveBoxContents = "maps";
+var gloveBoxContents = myStorage.car.inside["glove box"];
+
+console.log(gloveBoxContents)
