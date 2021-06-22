40732c0aa226f6e53b358b7acf80bfd56d458ed6
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-06-22 16:15:12 +0200

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/Testing-Objects-for-Properties.js
@@ -0,0 +1,8 @@
+function checkObj(obj, checkProp) {
+  if(obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+  // Only change code above this line
+}
