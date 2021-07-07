7fe67d9a4ab3a1fba14bb8333075cd7684c5269b
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-07-07 21:32:29 +0200

    .

---
+++ Data_Visualization/Data-Visualization-with-D3/Change-the-Presentation-of-a-Bar-Chart.js
@@ -0,0 +1,21 @@
+<style>
+  .bar {
+    width: 25px;
+    height: 100px;
+    margin: 2px;
+    display: inline-block;
+    background-color: blue;
+  }
+</style>
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    d3.select("body").selectAll("div")
+      .data(dataset)
+      .enter()
+      .append("div")
+      .attr("class", "bar")
+      .style("height", (d) => (d * 10 + "px"))
+  </script>
+</body>
