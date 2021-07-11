a09f2a74050a0d96372d82e325bafcc365c151ed
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-07-11 23:07:28 +0200

    .

--- Front_End_Development_Libraries/Bootstrap/Create-a-Custom-Heading.js
+++ Front_End_Development_Libraries/Bootstrap/Create-a-Custom-Heading.js
@@ -11,13 +11,14 @@
     border-style: solid;
     border-radius: 50%;
   }
-</style>
 
+</style>
 <div class="container-fluid">
   <div class="row">
     <div class="col-xs-8">
       <h2 class="text-primary  text-center">CatPhotoApp</h2>
     </div>
+
     <div class="col-xs-4">
         <a href="#"><img class="img-responsive thick-green-border" src="https://bit.ly/fcc-relaxing-cat" alt="A cute orange cat lying on its back."></a>
     </div>
