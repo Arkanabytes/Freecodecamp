b0f2787e202c9cf251ee5fb75686262be348972e
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-07-16 23:26:55 +0200

    .

--- Front_End_Development_Libraries/Bootstrap/Responsively-Style-Checkboxes.js
+++ Front_End_Development_Libraries/Bootstrap/Responsively-Style-Checkboxes.js
@@ -18,6 +18,7 @@
     <div class="col-xs-8">
       <h2 class="text-primary text-center">CatPhotoApp</h2>
     </div>
+
     <div class="col-xs-4">
       <a href="#"><img class="img-responsive thick-green-border" src="https://bit.ly/fcc-relaxing-cat" alt="A cute orange cat lying on its back."></a>
     </div>
