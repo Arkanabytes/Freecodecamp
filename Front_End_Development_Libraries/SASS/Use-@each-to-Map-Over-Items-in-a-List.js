db9170c5ea3d02f77de0b187be455aadb36c5158
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-08-02 23:05:50 +0200

    .

---
+++ Front_End_Development_Libraries/SASS/Use-@each-to-Map-Over-Items-in-a-List.js
@@ -0,0 +1,27 @@
+<style type='text/scss'>
+@each $color in blue, black, red {
+  .#{$color}-text {color: $color;}
+}
+
+  .blue-bg {
+  background-color: blue;
+}
+
+.black-bg {
+  background-color: black;
+}
+
+.red-bg {
+  background-color: red;
+}
+
+
+  div {
+    height: 200px;
+    width: 200px;
+  }
+</style>
+
+<div class="blue-bg"></div>
+<div class="black-bg"></div>
+<div class="red-bg"></div>
