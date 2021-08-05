cef7e9ef203438fd505e2a569963c482235ead05
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-08-05 22:44:02 +0200

    .

--- JavaScript_Algorithms_and_Data_Structures/ES6/Prevent-Object-Mutation.js
+++ JavaScript_Algorithms_and_Data_Structures/ES6/Prevent-Object-Mutation.js
@@ -2,6 +2,7 @@ function freezeObj() {
   const MATH_CONSTANTS = {
     PI: 3.14
   };
+  
   // Only change code below this line
   Object.freeze(MATH_CONSTANTS);
   // Only change code above this line
