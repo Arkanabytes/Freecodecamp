dc7ed8e4696e443fa3240c116729481e3a863cf7
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-09-02 22:15:37 +0200

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Access-Property-Names-with-Bracket-Notation.js
@@ -0,0 +1,15 @@
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // change code below this line
+  return foods[scannedItem];
+}
+
+console.log(checkInventory("apples"));
