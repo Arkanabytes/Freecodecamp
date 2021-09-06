745bcd3af3096a1caacd072e4f1aecfd1a406b56
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-09-06 23:41:22 +0200

    .

--- JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Add-Items-Using-splice().js
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Add-Items-Using-splice().js
@@ -1,7 +1,7 @@
 //setup
 function htmlColorNames(arr) {
   // Only change code below this line
- arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+  arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
   // Only change code above this line
   return arr;
 }
