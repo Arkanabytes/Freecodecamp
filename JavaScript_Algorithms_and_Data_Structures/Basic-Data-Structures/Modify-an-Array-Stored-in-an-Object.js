239a88bb3db9d237783e7ae92f1e66d5d9209ce6
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-09-08 22:45:46 +0200

    .

--- JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Modify-an-Array-Stored-in-an-Object.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Modify-an-Array-Stored-in-an-Object.js
@@ -21,8 +21,8 @@ let user = {
 
 function addFriend(userObj, friend) {
   // Only change code below this line
-  userObj.data.friends.push(friend);
-  return userObj.data.friends;
+   userObj.data.friends.push(friend);
+   return userObj.data.friends;
   // Only change code above this line
 }
 
