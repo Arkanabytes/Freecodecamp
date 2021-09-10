684d6c0076c41ef5e91f272be76c8ed69dea4d7c
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-09-10 23:28:02 +0200

    .

--- JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Create-complex-multi-dimensional-arrays.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Create-complex-multi-dimensional-arrays.js
@@ -1,5 +1,6 @@
 //setup
 let myNestedArray = [
+  
   // Only change code below this line
   'level 1',                   /* myNestedArray[0]             */
   ['level 2'],	               /* myNestedArray[1][0]          */
@@ -7,4 +8,5 @@ let myNestedArray = [
   [[['level 4','deeper']]],    /* myNestedArray[3][0][0][0]    */
   [[[['level 5','deepest']]]]  /* myNestedArray[4][0][0][0][0] */
   // Only change code above this line
+  
 ];
