15abe374c89d4b31d70800d243cd028279acb6fe
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-09-12 21:55:13 +0200

    .

--- JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Copy-an-Array-with-the-Spread-Operator.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Copy-an-Array-with-the-Spread-Operator.js
@@ -3,6 +3,7 @@ function copyMachine(arr, num) {
   
   let newArr = [];
   while (num >= 1) {
+    
     // Only change code below this line
     newArr.push([...arr]);
     // Only change code above this line
