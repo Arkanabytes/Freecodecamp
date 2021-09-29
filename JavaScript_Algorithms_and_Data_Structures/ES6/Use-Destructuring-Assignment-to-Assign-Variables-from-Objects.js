07590a64802615ded4d8df20fdd57571c990b96a
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-09-29 20:46:47 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/ES6/Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
+++ JavaScript_Algorithms_and_Data_Structures/ES6/Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -3,7 +3,6 @@ const HIGH_TEMPERATURES = {
   today: 77,
   tomorrow: 80
 };
-
 // Only change code below this line
 const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
 // Only change code above this line
