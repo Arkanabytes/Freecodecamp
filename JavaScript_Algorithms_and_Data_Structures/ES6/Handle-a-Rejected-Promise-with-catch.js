955524b1c377ad009b5eef53415691f281071d42
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-09-30 23:02:37 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/ES6/Handle-a-Rejected-Promise-with-catch.js
+++ JavaScript_Algorithms_and_Data_Structures/ES6/Handle-a-Rejected-Promise-with-catch.js
@@ -1,7 +1,6 @@
 const makeServerRequest = new Promise((resolve, reject) => {
   // responseFromServer is set to false to represent an unsuccessful response from a server
   let responseFromServer = false;
-	
   if(responseFromServer) {
     resolve("We got the data");
   } else {	
