142222f6fe791622b41365ef5cccda4aa04505e6
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-10-06 23:07:46 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/ES6/Complete-a-Promise-with-resolve-and-reject.js
+++ JavaScript_Algorithms_and_Data_Structures/ES6/Complete-a-Promise-with-resolve-and-reject.js
@@ -2,7 +2,6 @@
 const makeServerRequest = new Promise((resolve, reject) => {
   // responseFromServer represents a response from a server
   let responseFromServer;
-
   if(responseFromServer) {
     resolve("We got the data");
   } else {	
