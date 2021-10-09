330b94a2de7e5999132de5f92583e08635352170
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-10-08 23:18:06 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/ES6/Use-*-to-Import-Everything-from-a-File.js
+++ JavaScript_Algorithms_and_Data_Structures/ES6/Use-*-to-Import-Everything-from-a-File.js
@@ -1,6 +1,5 @@
 //setup
 import * as stringFunctions from "./string_functions.js";
 // add code above this line
-
 stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
