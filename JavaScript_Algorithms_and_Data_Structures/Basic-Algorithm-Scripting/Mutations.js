b222d51903a880053db82ce17f625332bbc63aa2
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-10-15 03:21:06 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Basic-Algorithm-Scripting/Mutations.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Algorithm-Scripting/Mutations.js
@@ -2,7 +2,7 @@
 function mutation(arr) {
    return arr[1]
        .toLowerCase()
-    .split("")
+       .split("")
     .every(function(letter) {
       return arr[0].toLowerCase().indexOf(letter) != -1;
     });
