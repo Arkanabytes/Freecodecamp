d67a21bc845abdd0bf0c86752895d73e3469829a
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-10-16 22:28:55 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Basic-Algorithm-Scripting/Factorialize-a-Number.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Algorithm-Scripting/Factorialize-a-Number.js
@@ -2,7 +2,7 @@
 function factorialize(num, factorial = 1) {
     return num < 0 ? 1 : (
      new Array(num)
-      .fill(undefined)
+        .fill(undefined)
       .reduce((product, val, index) => product * (index + 1), 1)
   );
 }
