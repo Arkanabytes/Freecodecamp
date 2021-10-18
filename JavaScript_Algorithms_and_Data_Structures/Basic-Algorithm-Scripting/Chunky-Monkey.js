4115b1e4214396a124e9b87b287f0c4298652037
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-10-17 21:02:37 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Basic-Algorithm-Scripting/Chunky-Monkey.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Algorithm-Scripting/Chunky-Monkey.js
@@ -2,7 +2,7 @@
 function chunkArrayInGroups(arr, size){
         if (arr.length <= size) {
                 return [arr];
-    } else {
+         } else {
                return [arr.slice(0, size)].concat(
                 chunkArrayInGroups(arr.slice(size), size)
     );
