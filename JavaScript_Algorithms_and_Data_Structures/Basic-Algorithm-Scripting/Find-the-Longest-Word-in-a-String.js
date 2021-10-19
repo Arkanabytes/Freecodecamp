6285e6e081f0b8c866aaf41db645fa0f3f780b95
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-10-18 23:23:24 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Basic-Algorithm-Scripting/Find-the-Longest-Word-in-a-String.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Algorithm-Scripting/Find-the-Longest-Word-in-a-String.js
@@ -2,7 +2,6 @@
 function findLongestWordLength(str) {
   let words = str.split(' ');
   let maxLength = 0;
-
   for (let i = 0; i < words.length; i++) {
     if (words[i].length > maxLength) {
       maxLength = words[i].length;
