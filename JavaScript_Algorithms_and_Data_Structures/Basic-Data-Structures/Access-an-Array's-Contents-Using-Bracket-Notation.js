81162c0fdfef9f1e44cf09a5f8a21a3a52b84c2b
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-10-29 21:44:48 -0300

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic-Data-Structures/Access-an-Array's-Contents-Using-Bracket-Notation.js
@@ -0,0 +1,6 @@
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "anything we want";
+// Only change code below this line
+//myArray[1] = "not b anymore";
+// Only change code above this line
+console.log(myArray);
