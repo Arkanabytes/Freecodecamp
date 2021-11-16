f2cead00ba4c574969c5dd94649a85646d9996f5
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-11-15 21:55:15 -0300

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Object-Oriented-Programming/Remember-to-Set-the-Constructor-Property-when-Changing-the-Prototype.js
@@ -0,0 +1,17 @@
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+
+Dog.prototype = {
+  constructor: Dog, // Solution
+
+  numLegs: 2,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
