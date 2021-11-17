dfcd0bbde410d069eadff6e997738372ec22bb0b
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-11-17 18:11:29 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Object-Oriented-Programming/Understand-the-Prototype-Chain.js
+++ JavaScript_Algorithms_and_Data_Structures/Object-Oriented-Programming/Understand-the-Prototype-Chain.js
@@ -4,7 +4,6 @@ function Dog(name) {
 }
 
 let beagle = new Dog("Snoopy");
-
 Dog.prototype.isPrototypeOf(beagle); // => true
 // Fix the code below so that it evaluates to true
 Object.prototype.isPrototypeOf(Dog.prototype);
