ca93e9627a47ee8cb21bdabe977ba9be83e39b12
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-11-26 21:59:00 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Object-Oriented-Programming/Override-Inherited-Methods.js
+++ JavaScript_Algorithms_and_Data_Structures/Object-Oriented-Programming/Override-Inherited-Methods.js
@@ -9,7 +9,6 @@ Penguin.prototype.constructor = Penguin;
 Penguin.prototype.fly = function() {
   return "Alas, this is a flightless bird.";
 };
-
 // Only change code above this line
 let penguin = new Penguin();
 console.log(penguin.fly());
