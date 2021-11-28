e59aa9eacb542d13d5767230615f1f6cc4baed7e
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-11-28 17:31:32 -0300

    .

---
+++ JavaScript_Algorithms_and_Data_Structures/Object-Oriented-Programming/Use-an-IIFE-to-Create-a-Module.js
@@ -0,0 +1,11 @@
+let funModule = ( () => {
+  return {
+    isCuteMixin: (obj) => {
+      obj.isCute = () => { true; };
+    },
+    singMixin: (obj) => {
+      obj.sing = () => { console.log("Singing to an awesome tune"); }
+    }
+
+  }
+})();
