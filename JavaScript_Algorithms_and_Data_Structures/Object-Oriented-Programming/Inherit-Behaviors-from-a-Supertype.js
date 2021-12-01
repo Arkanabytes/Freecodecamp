bd93f0869873230040d8a9fad719074b9a01d0ab
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-11-30 22:55:03 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Object-Oriented-Programming/Inherit-Behaviors-from-a-Supertype.js
+++ JavaScript_Algorithms_and_Data_Structures/Object-Oriented-Programming/Inherit-Behaviors-from-a-Supertype.js
@@ -6,7 +6,6 @@ Animal.prototype = {
     console.log("nom nom nom");
   }
 };
-
 // Only change code below this line
 let duck = Object.create(Animal.prototype); // Change this line
 let beagle = Object.create(Animal.prototype); // Change this line
