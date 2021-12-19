d95b35181655be77f77f28326e26c66fb9453828
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-12-19 19:53:03 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/ES6/Handle-a-Fulfilled-Promise-with-then.js
+++ JavaScript_Algorithms_and_Data_Structures/ES6/Handle-a-Fulfilled-Promise-with-then.js
@@ -1,4 +1,4 @@
-//setup
+//Setup
 const makeServerRequest = new Promise((resolve, reject) => {
   // responseFromServer is set to true to represent a successful response from a server
   let responseFromServer = true;	
