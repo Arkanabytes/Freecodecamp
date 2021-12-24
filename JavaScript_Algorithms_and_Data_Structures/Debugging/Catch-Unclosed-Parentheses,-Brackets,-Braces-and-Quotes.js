bd773c14ef5676dd39ec7c348f901e680c76cc66
Alejandra Pinto <cpinto36@alumno.uned.es>
2021-12-23 22:32:02 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Debugging/Catch-Unclosed-Parentheses,-Brackets,-Braces-and-Quotes.js
+++ JavaScript_Algorithms_and_Data_Structures/Debugging/Catch-Unclosed-Parentheses,-Brackets,-Braces-and-Quotes.js
@@ -1,4 +1,4 @@
-//Setup
+//Setup Arkanabytes
 let myArray = [1, 2, 3];
 let arraySum = myArray.reduce((previous, current) =>  previous + current);
 console.log(`Sum of array values is: ${arraySum}`);
