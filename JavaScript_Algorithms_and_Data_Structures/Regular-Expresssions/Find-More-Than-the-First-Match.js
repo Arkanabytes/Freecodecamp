2265c049eb2d4d241eec2530137152301008e3de
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-01-27 23:22:45 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Find-More-Than-the-First-Match.js
+++ JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Find-More-Than-the-First-Match.js
@@ -1,4 +1,4 @@
-//setup
+//Setup
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle/gi; // Change this line
 let result = twinkleStar.match(starRegex); // Change this line
