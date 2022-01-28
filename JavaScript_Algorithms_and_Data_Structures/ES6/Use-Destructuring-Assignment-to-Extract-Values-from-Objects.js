191fc7cdcd25fdd32707d739daf863bcdc374ae3
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-01-27 23:20:49 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/ES6/Use-Destructuring-Assignment-to-Extract-Values-from-Objects.js
+++ JavaScript_Algorithms_and_Data_Structures/ES6/Use-Destructuring-Assignment-to-Extract-Values-from-Objects.js
@@ -4,7 +4,6 @@ const HIGH_TEMPERATURES = {
   today: 77,
   tomorrow: 80
 };
-
 // Only change code below this line
 const {today, tomorrow} = HIGH_TEMPERATURES;
 // Only change code above this line
