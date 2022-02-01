f599b63f631d652cc1782bfb6bf7dbe9e7b29d4a
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-01-31 21:36:32 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Debugging/Understanding-the-Differences-between-the-freeCodeCamp-and-Browser-Console.js
+++ JavaScript_Algorithms_and_Data_Structures/Debugging/Understanding-the-Differences-between-the-freeCodeCamp-and-Browser-Console.js
@@ -1,4 +1,4 @@
-//Setup
+//Setup Arkanabytes
 // Open your browser console.
 let output = "Get this to log once in the freeCodeCamp console and twice in the browser console";
 // Use console.log() to print the output variable.
