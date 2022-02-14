d9f840bc88434576b62bb1c72e23d77c15bd2d85
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-02-13 23:13:57 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Non-Whitespace-Characters.js
+++ JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Non-Whitespace-Characters.js
@@ -1,4 +1,4 @@
-//Setup
+//Setup Arkanabytes
 let sample = "Whitespace is important in separating words";
 let countNonWhiteSpace = /\S/g; // Change this line
 let result = sample.match(countNonWhiteSpace);
