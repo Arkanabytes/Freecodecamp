54e90e1821642d85dd7451ceed38cef3c69db376
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-02-16 22:32:15 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Single-Character-with-Multiple-Possibilities.js
+++ JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Single-Character-with-Multiple-Possibilities.js
@@ -1,4 +1,4 @@
-//Setup
+//Setup Arkanabytes
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // Change this line
 let result = quoteSample.match(vowelRegex); // Change this line
