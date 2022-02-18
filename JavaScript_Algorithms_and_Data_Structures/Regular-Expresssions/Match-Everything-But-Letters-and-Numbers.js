125a8eb9929dc35348eef34551e56f183edd5f75
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-02-17 22:14:49 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Everything-But-Letters-and-Numbers.js
+++ JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Everything-But-Letters-and-Numbers.js
@@ -1,4 +1,4 @@
-//setup
+//Setup
 let quoteSample = "The five boxing wizards jump quickly.";
 let nonAlphabetRegex = /\W/g; // Change this line
 let result = quoteSample.match(nonAlphabetRegex).length;
