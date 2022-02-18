478a319086a0ac9ef0ce7eb8aaaead17f4369efc
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-02-17 22:15:45 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Extract-Matches.js
+++ JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Extract-Matches.js
@@ -1,4 +1,4 @@
-//Setup
+//Setup Arkanabytes
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result = extractStr.match(codingRegex); // Change this line
