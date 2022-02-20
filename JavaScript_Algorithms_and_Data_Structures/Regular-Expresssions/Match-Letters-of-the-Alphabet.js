cb7a521f6490ea30bd28f97e4d735a65b54b01a0
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-02-19 22:51:21 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Letters-of-the-Alphabet.js
+++ JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Letters-of-the-Alphabet.js
@@ -1,4 +1,4 @@
-//Setup
+//Setup Arkanabytes
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi; // Change this line
 let result = quoteSample.match(alphabetRegex); // Change this line
