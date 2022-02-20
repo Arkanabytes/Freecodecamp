757883d278f27bff290240d313a8d0c058cee999
Alejandra Pinto <cpinto36@alumno.uned.es>
2022-02-19 22:50:37 -0300

    .

--- JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Whitespace.js
+++ JavaScript_Algorithms_and_Data_Structures/Regular-Expresssions/Match-Whitespace.js
@@ -1,4 +1,4 @@
-//Setup
+//Setup Arkanabytes
 let sample = "Whitespace is important in separating words";
 let countWhiteSpace = /\s/g; // Change this line
 let result = sample.match(countWhiteSpace);
